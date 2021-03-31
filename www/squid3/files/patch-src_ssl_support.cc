e7ad26d92beff76bbead7b6b675ad5c551e86fa9
Shawn Webb <shawn.webb@hardenedbsd.org>
2021-03-31 10:29:24 -0400

    HBSD: Resolve merge conflicts
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- www/squid3/files/patch-src_ssl_support.cc
+++ www/squid3/files/patch-src_ssl_support.cc
@@ -1,26 +1,15 @@
 --- src/ssl/support.cc.orig	2018-07-15 20:46:55 UTC
 +++ src/ssl/support.cc
-<<<<<<< HEAD
-@@ -2159,7 +2159,11 @@ remove_session_cb(SSL_CTX *, SSL_SESSION
-=======
 @@ -2159,7 +2159,11 @@ remove_session_cb(SSL_CTX *, SSL_SESSION *sessionID)
->>>>>>> upstream/master
  }
  
  static SSL_SESSION *
 -get_session_cb(SSL *, unsigned char *sessionID, int len, int *copy)
 +get_session_cb(SSL *,
-<<<<<<< HEAD
-+#if defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER >= 0x20800000L
-+const
-+#endif
-+unsigned char *sessionID, int len, int *copy)
-=======
 +#ifdef LIBRESSL_VERSION_NUMBER
 +  const
 +#endif
 +  unsigned char *sessionID, int len, int *copy)
->>>>>>> upstream/master
  {
      if (!SslSessionCache)
          return NULL;
